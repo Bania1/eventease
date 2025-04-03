c3be8a0c9cb06cfbac5c348641ce258e72089dfb
Angel Bauste <denicoangel@gmail.com>
2025-04-03 10:37:17 +0300

    app_folder

---
+++ eventease-app/eventease-app/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eventease_app.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "About Page";
+            return View();
+        }
+    }
+}
