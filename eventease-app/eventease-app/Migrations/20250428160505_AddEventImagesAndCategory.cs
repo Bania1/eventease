a03db306301a7cb84a587db1b6c2352655f97a67
Angel Bauste <denicoangel@gmail.com>
2025-04-28 22:20:02 +0300

    partial frontend and functionalities for user and event V2

---
+++ eventease-app/eventease-app/Migrations/20250428160505_AddEventImagesAndCategory.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eventease_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventImagesAndCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeroFileName",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LongDescription",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailFileName",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "HeroFileName",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "LongDescription",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ThumbnailFileName",
+                table: "Events");
+        }
+    }
+}
