ed88f93f332c3980da8e5ed096155812ca70ad70
Angel Eduardo Bauste <115702480+Bania1@users.noreply.github.com>
2025-05-06 01:35:05 +0200

    Add event create button and QR code functionality (#14)

---
+++ eventease-app/eventease-app/QRCoder/QRCode.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Drawing;
+
+namespace QRCoder
+{
+    internal class QRCode
+    {
+        private QRCodeData qrCodeData;
+
+        public QRCode(QRCodeData qrCodeData)
+        {
+            this.qrCodeData = qrCodeData;
+        }
+
+        internal void Dispose()
+        {
+            if (qrCodeData != null)
+            {
+                qrCodeData.Dispose(); // Only if QRCodeData implements IDisposable
+                qrCodeData = null;
+            }
+        }
+
+        internal Bitmap GetGraphic(int pixelsPerModule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
